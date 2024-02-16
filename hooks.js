f524656ed0a8ceb871efb28c3bafe55f4184fa5b
HalfLife7 <d.nguyen@vehikl.com>
2024-02-16 18:46:29 -0500

    refactor to use react-use-websocket

--- hooks.js
+++ hooks.js
@@ -1,69 +1,75 @@
-import { useState, useEffect } from 'react';
+import {useEffect, useState} from 'react';
+import useWebSocket from "react-use-websocket";
 
-export const useSocket = () => {
-    const [socket, setSocket] = useState(new WebSocket('ws://localhost:3000/cable'));
-    const [xPositionFromSocket, setXPositionFromSocket] = useState(null);
-    const [yPositionFromSocket, setYPositionFromSocket] = useState(null);
+export const useSocket = (socketUrl) => {
+    const [xPositionFromSocket, setXPositionFromSocket] = useState(null)
+    const [yPositionFromSocket, setYPositionFromSocket] = useState(null)
+    const handleSocketMessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === "ping") {
+            return;
+        }
 
+        // set position of the alert from socket
+        if (data?.message?.type === "alert") {
+            setXPositionFromSocket(data.message.position.x)
+            setYPositionFromSocket(data.message.position.y)
+        }
+
+        // debugging
+        // if (data.message) {
+        //     console.log('Received data from server', event.data);
+        // }
+    };
+
+    const handleSocketClose = (event) => {
+        console.log('Disconnected from server');
+    };
+
+    const handleSocketError = (error) => {
+        console.log('WebSocket error observed: ', error);
+    };
+
+    const {
+        sendMessage,
+        lastMessage,
+        readyState,
+        sendJsonMessage,
+        lastJsonMessage
+    } = useWebSocket(socketUrl,
+        {
+            onOpen: () => {
+                console.log('Connected to the server');
+
+                const message = {
+                    command: 'subscribe',
+                    identifier: JSON.stringify({
+                        channel: 'VisualAlertChannel',
+                    }),
+                };
+
+                sendJsonMessage(message);
+            },
+            shouldReconnect: () => true,
+            onMessage: (event) => handleSocketMessage(event),
+            onClose: () => handleSocketClose,
+            onError: () => handleSocketError
+        });
+
+    return {
+        sendMessage,
+        lastMessage,
+        readyState,
+        sendJsonMessage,
+        lastJsonMessage,
+        xPositionFromSocket,
+        yPositionFromSocket
+    };
+}
+
+export const useVisualAlert = (sendJsonMessage) => {
     useEffect(() => {
-        const handleSocketOpen = (event) => {
-            console.log('Connected to the server');
 
-            const message = {
-                command: 'subscribe',
-                identifier: JSON.stringify({
-                    channel: 'VisualAlertChannel',
-                }),
-            };
-
-            socket.send(JSON.stringify(message));
-        };
-
-        const handleSocketMessage = (event) => {
-            const data = JSON.parse(event.data);
-            if (data.type === 'ping') {
-                return;
-            }
-
-            // set position of the alert from socket
-            if (data?.message?.type === 'alert') {
-                setXPositionFromSocket(data.message.position.x);
-                setYPositionFromSocket(data.message.position.y);
-            }
-
-            // debugging
-            // if (data.message) {
-            //     console.log('Received data from server', event.data);
-            // }
-        };
-
-        const handleSocketClose = (event) => {
-            console.log('Disconnected from server');
-        };
-
-        const handleSocketError = (error) => {
-            console.log('WebSocket error observed: ', error);
-        };
-
-        socket.addEventListener('open', handleSocketOpen);
-        socket.addEventListener('message', handleSocketMessage);
-        socket.addEventListener('close', handleSocketClose);
-        socket.addEventListener('error', handleSocketError);
-
-        // Close the socket when the component unmounts
-        return () => {
-            console.log('Closing WebSocket connection');
-            if (socket.readyState === WebSocket.OPEN) {
-                socket.close();
-            }
-        };
-    }, [socket]);
-
-    return { xPositionFromSocket, yPositionFromSocket, socket, setSocket };
-};
-
-export const useVisualAlert = (socket) => {
-    useEffect(() => {
         const handleVisualAlert = (event) => {
             if (event.ctrlKey) {
                 const message = {
@@ -71,10 +77,10 @@ export const useVisualAlert = (socket) => {
                     identifier: JSON.stringify({
                         channel: 'VisualAlertChannel',
                     }),
-                    data: JSON.stringify({ action: "visual_alert", position: {x: event.pageX, y: event.pageY} })
+                    data: JSON.stringify({action: "visual_alert", position: {x: event.pageX, y: event.pageY}})
                 };
 
-                socket.send(JSON.stringify(message));
+                sendJsonMessage(message);
             }
         }
 
@@ -82,5 +88,5 @@ export const useVisualAlert = (socket) => {
         return () => {
             document.removeEventListener('click', handleVisualAlert);
         }
-    }, [socket]);
+    }, []);
 }
